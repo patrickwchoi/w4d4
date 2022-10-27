c586805109456a28d467ca920d190a4e1ee319d7
Patrick Choi <ptrckchoi@gmail.com>
2022-10-27 15:40:04 -0700

    Working on Towers of Hanoi

--- tdd_project/lib/tdd_project.rb
+++ tdd_project/lib/tdd_project.rb
@@ -16,4 +16,75 @@ class Array
         end
         res
     end
+
+    def my_transpose
+        result = []
+        (0...self.length).each do |i|
+            new_row = []
+            (0...self.length).each do |j|
+                new_row << self[j][i]
+            end
+            result << new_row
+        end
+        result
+    end
+
+    def stock_picker
+        result = 0
+        result_arr = []
+        (0...self.length).each do |i|
+            (i+1...self.length).each do |j|
+                if (self[j]-self[i]) > result
+                    result = self[j]-self[i] 
+                    result_arr = [i,j]
+                end
+            end
+        end
+        result_arr
+    end
+
 end
+
+
+class Towers
+    #instance var for array => an array of 3 arrays
+    #@tower1 = [["-"], ["-", "-"], ["-", "-", "-"]]
+        attr_accessor :towers
+    def initialize(n)
+        @towers = [[], [], [] ]
+        arr = (1..n).to_a
+        towers[0] = arr
+    end
+
+    def valid_start?(pos)
+
+    end
+
+    def valid_end?(size, pos)
+
+    end
+        # this will represent our tower 
+        # ["-"] - length 1
+        #["-", "-"] - length 2
+        #["-", "-", "-"] length 3
+end
+
+class Player
+    # 1 Step game start input = gets.chomp  choose the tower and we put hte first element in a tower of your choice [1,2,3] [ ] [ ]
+    def get_start_input
+        puts "Choose tower to pick first disk from"
+        start_input = gets.chomp.to_i
+        return start_input
+    end
+
+    def get_end_input
+        puts "Choose tower to place disk on"
+        end_input = gets.chomp.to_i
+        return end_input
+    end
+end
+
+class Game
+    #Player.get_end_input is not valid => we ask user to reenter data
+end
+
--- tdd_project/spec/tdd_project_spec.rb
+++ tdd_project/spec/tdd_project_spec.rb
@@ -13,4 +13,30 @@ describe 'Array' do
             expect([-1, 0, 2, -2, 1].two_sum).to eq([[0, 4], [2, 3]])
         end
     end
+
+    describe "#my_transpose" do
+        it "transpose original square matrix" do
+            rows = [
+                [0, 1, 2],
+                [3, 4, 5],
+                [6, 7, 8]
+            ]
+            cols = [
+                [0, 3, 6],
+                [1, 4, 7],
+                [2, 5, 8]
+              ]
+            expect(rows.my_transpose).to eq(cols)
+        end
+    end
+
+    describe "#stock_picker" do
+        #arr = [80,90,50,110,40] ==> [2,3]
+        #create anrray populated with 7 different prices as elements
+        it "picks most profitable pair of days as indices" do
+            stocks = [80,90,50,110,40] 
+            expect(stocks.stock_picker).to eq([2,3])
+        end
+    end
+
 end
